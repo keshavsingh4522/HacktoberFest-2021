feed6c789082fe8f2e07cc8a09c56f02d8b93751
Sudhakar B <56016120+SudhaKarRanji@users.noreply.github.com>
2021-10-09 17:41:52 +0530

    Create SimpepyQt.py

---
+++ SimpepyQt.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication, QLabel
+app = QApplication([])
+label = QLabel("HEllo world")
+label = QLabel("------**Hacktober Fest 2021**-------")
+label = QLabel("------**Hacktober Fest 2021**-------")
+#displays a label format for GUI
+label.show()
+
+app.exec_()
